0433f7e6c9960246706227301db65c5b69b7bb27
postlude <endigate@naver.com>
2017-08-31 15:56:54 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,16 @@
-//  OpenShift sample Node application
-var express = require('express'),
-    fs      = require('fs'),
-    app     = express(),
-    eps     = require('ejs'),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
+'use strict';
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+const express = require('express');
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+// 상수
+const PORT = 8080;
+const HOST = '0.0.0.0';
 
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
+// 앱
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
 });
 
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app ;
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
