7e35a4abf50b289716aaad8cd02365e0799020f2
postlude <endigate@naver.com>
2017-09-01 17:01:22 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
-LD_LIBRARY_PATH="/opt/rh/rh-nodejs6/root/usr/lib64"
+##LD_LIBRARY_PATH="/opt/rh/rh-nodejs6/root/usr/lib64"
 
-PATH=${PATH}:/opt/rh/rh-nodejs6/root/usr/bin
+##PATH=${PATH}:/opt/rh/rh-nodejs6/root/usr/bin
 
-export LD_LIBRARY_PATH PATH
+##export LD_LIBRARY_PATH PATH
 
 node -v
 
